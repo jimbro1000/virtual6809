2b6ac9fa9b68bbad68377864b9adca715c029785
Julian Brown <julian.brown@the-lair.info>
2021-02-06 13:27:50 +0000

    chore: housekeeping code style

--- src/alu/alu.js
+++ src/alu/alu.js
@@ -1,60 +1,72 @@
-class alu {
-    constructor(control_register) {
-        this.cc = control_register;
-    }
+/**
+ * Creates an arithmetic logic unit
+ * @class
+ */
+class Alu {
+  /**
+   * @constructor
+   * @param {ControlRegister} controlRegister
+   */
+  constructor(controlRegister) {
+    this.cc = controlRegister;
 
-    add8 = (reg1, reg2, c) => {
-        if (c === undefined) { c = 0; }
-        const result = (reg1 + reg2 + c);
-        const masked = result & 0xff;
-        const half = (reg1 & 0xf) + (reg2 & 0xf) + c;
-        const sign_mask = (reg1 ^ reg2) ^ 0x80;
-        this.cc.negative(result & 0x80 > 0);
-        this.cc.carry(result !== masked);
-        this.cc.zero(masked === 0);
-        this.cc.halfcarry(half > 0xf);
-        this.cc.overflow((sign_mask & (reg2 ^ masked)) & 0x80);
-        return masked;
-    }
+    this.add8 = (reg1, reg2, c) => {
+      if (c === undefined) {
+        c = 0;
+      }
+      const result = (reg1 + reg2 + c);
+      const masked = result & 0xff;
+      const half = (reg1 & 0xf) + (reg2 & 0xf) + c;
+      const signMask = (reg1 ^ reg2) ^ 0x80;
+      this.cc.negative((result & 0x80) !== 0);
+      this.cc.carry(result !== masked);
+      this.cc.zero(masked === 0);
+      this.cc.halfcarry(half > 0xf);
+      this.cc.overflow(((signMask & (reg2 ^ masked)) & 0x80) !== 0);
+      return masked;
+    };
 
-    add16 = (reg1, reg2) => {
-        const result = (reg1 + reg2);
-        const masked = result & 0xffff;
-        const sign_mask = (reg1 ^ reg2) ^ 0x8000;
-        this.cc.negative(result & 0x8000 > 0);
-        this.cc.carry(result !== masked);
-        this.cc.zero(masked === 0);
-        this.cc.overflow((sign_mask & (reg2 ^ masked)) & 0x8000);
-        return masked;
-    }
+    this.add16 = (reg1, reg2) => {
+      const result = (reg1 + reg2);
+      const masked = result & 0xffff;
+      const signMask = (reg1 ^ reg2) ^ 0x8000;
+      this.cc.negative((result & 0x8000) !== 0);
+      this.cc.carry(result !== masked);
+      this.cc.zero(masked === 0);
+      this.cc.overflow(((signMask & (reg2 ^ masked)) & 0x8000) !== 0);
+      return masked;
+    };
 
-    sub8 = (reg1, reg2, c) => {
-        if (c === undefined) { c = 0; }
-        const result = reg1 - reg2 - c;
-        const masked = result & 0xff;
-        let sign_mask;
-        if (c === 0) {
-            sign_mask = (reg1 ^ reg2);
-        } else {
-            sign_mask = (reg1 ^ reg2) ^ 0x80;
-        }
-        this.cc.negative(result & 0x80 > 0);
-        this.cc.carry(result !== masked);
-        this.cc.zero(masked === 0);
-        this.cc.overflow((sign_mask & (reg1 ^ masked)) & 0x80);
-        return masked;
-    }
+    this.sub8 = (reg1, reg2, c) => {
+      if (c === undefined) {
+        c = 0;
+      }
+      const result = reg1 - reg2 - c;
+      const masked = result & 0xff;
+      let signMask;
+      if (c === 0) {
+        signMask = (reg1 ^ reg2);
+      } else {
+        signMask = (reg1 ^ reg2) ^ 0x80;
+      }
+      this.cc.negative((result & 0x80) !== 0);
+      this.cc.carry(result !== masked);
+      this.cc.zero(masked === 0);
+      this.cc.overflow(((signMask & (reg1 ^ masked)) & 0x80) !== 0);
+      return masked;
+    };
 
-    sub16 = (reg1, reg2) => {
-        const result = reg1 - reg2;
-        const masked = result & 0xffff;
-        const sign_mask = (reg1 ^ reg2);
-        this.cc.negative(result & 0x8000 > 0);
-        this.cc.carry(result !== masked);
-        this.cc.zero(masked === 0);
-        this.cc.overflow((sign_mask & (reg1 ^ masked)) & 0x8000);
-        return masked;
-    }
+    this.sub16 = (reg1, reg2) => {
+      const result = reg1 - reg2;
+      const masked = result & 0xffff;
+      const signMask = (reg1 ^ reg2);
+      this.cc.negative((result & 0x8000) !== 0);
+      this.cc.carry(result !== masked);
+      this.cc.zero(masked === 0);
+      this.cc.overflow(((signMask & (reg1 ^ masked)) & 0x8000) !== 0);
+      return masked;
+    };
+  }
 }
 
-module.exports = { Alu: alu }
+module.exports = {Alu};
